c8ed107c2f1d056ec0d75fbcff34cfc6d6c7810d
Dru Kelly <drukelly@gmail.com>
2019-05-12 22:46:06 -0700

    Update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,19 +14,35 @@ const connection = mysql.createConnection({
 connection.connect(function (error) {
   if (error) throw error
   // console.log(`connected as id ${connection.threadId}`)
-  queryAll()
+  listInventory()
 })
 
-function queryAll () {
+function listInventory () {
   let query = `select * from ${process.env.dbTable}`
   connection.query(query, function (error, response) {
     if (error) throw error
-    const table = new Table({
+    let table = new Table({
       head: ['SKU', 'Product Name', 'Department', 'Price', 'In Stock']
     })
     for (let i = 0; i < response.length; i++) {
       table.push([`${response[i].sku}`, `${response[i].product_name}`, `${response[i].department_name}`, `${response[i].price}`, `${response[i].stock_quantity}`])
     }
     console.log(table.toString())
+    inquirer.prompt({
+      name: 'selectedSKU',
+      type: 'input',
+      message: 'Which SKU item would you like to buy?'
+    })
+    .then(function (itemToBuy) {
+      // console.log(itemToBuy.selectedSKU)
+      inquirer.prompt( {
+        name: 'selectedQty',
+        type: 'input',
+        message: 'How many would you like?'
+      }).then(function (qty) {
+        // console.log(qty.selectedQty)
+        console.log(`SKU: ${itemToBuy.selectedSKU}, QTY: ${qty.selectedQty}`);
+      })
+    })
   })
 }
